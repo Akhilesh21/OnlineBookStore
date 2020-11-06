719baaf7c1148a2be991df90025431f309d29527
Akhilesh21 <akhilesh.mc4@gmail.com>
2020-11-07 00:00:29 +0530

    [Akhilesh] added login and logout funtionality

--- bookstoreapp/resources/views/header.blade.php
+++ bookstoreapp/resources/views/header.blade.php
@@ -1,6 +1,11 @@
 <?php
 use App\Http\Controllers\ProductController;
+$total =0;
+if(Session::has('user'))
+{
 $total = ProductController::cartItem();
+}
+
 ?>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
@@ -13,7 +18,7 @@ $total = ProductController::cartItem();
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">BookStore</a>
+            <a class="navbar-brand" href="/">BookStore</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -30,6 +35,17 @@ $total = ProductController::cartItem();
             </form>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Cart({{$total}})</a></li>
+                @if(Session::has('user'))
+                <li class="dropdown">
+                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+                 <span class="caret"></span></a>
+                   <ul class="dropdown-menu">
+              <li><a href="/logout">logout</a></li>
+        </ul>
+      </li>
+      @else
+      <li><a href="/login">Login</a></li>
+      @endif
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
