d418ec55f35ffea3ac12eb68c8e34a731ca63a7b
Akhilesh21 <akhilesh.mc4@gmail.com>
2020-11-09 16:50:04 +0530

    [Akhilesh] added created order model

--- bookstoreapp/resources/views/cartlist.blade.php
+++ bookstoreapp/resources/views/cartlist.blade.php
@@ -5,6 +5,7 @@
 <div class="col-sm-10">
 <div class="trending-wrapper">
         <h4>Results for Books</h4>
+        <a class="btn btn-success" href="ordernow">Order Now</a> <br><br>
         @foreach($products as $item)
         <div class="row searched-item cart-list-devider">
             <div class="col-sm-3">
@@ -19,11 +20,13 @@
                 </div>
             </div>
             <div class="col-sm-3">
+
               <a href="/removecart/{{$item->cart_id}}" class="btn btn-warning">Remove to cart</a> 
             </div>
         </div>
         @endforeach
     </div>
+    <a class="btn btn-success" href="ordernow">Order Now</a> <br><br>
 </div>
 
 </div>
